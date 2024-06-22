92fc2b14b886125231738fadc59098c152222129
goeun kim <rlarhdms6828@naver.com>
2024-06-22 23:54:52 +0900

    Section 17

---
+++ 17 Forms User Input/src/components/RefLogin.jsx
@@ -0,0 +1,50 @@
+import { useRef, useState } from 'react';
+
+export default function Login() {
+  const [emailIsInvalid, setEmailIsInvalid] = useState(false);
+  const emailRef = useRef();
+  const passwordRef = useRef();
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    const enteredEmail = emailRef.current.value;
+
+    console.log(enteredEmail);
+    const emailIsValid = enteredEmail.includes('@');
+
+    if (!emailIsValid) {
+      setEmailIsInvalid(true);
+      return;
+    }
+
+    setEmailIsInvalid(false);
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2>Login</h2>
+
+      <div className="control-row">
+        <div className="control no-margin">
+          <label htmlFor="email">Email</label>
+          <input id="email" type="email" name="email" ref={emailRef} />
+          <div className="control-error">
+            {emailIsInvalid && <p>Please enter a valid email</p>} {/* Corrected the error message */}
+          </div>
+        </div>
+
+        <div className="control no-margin">
+          <label htmlFor="password">Password</label>
+          <input id="password" type="password" name="password" ref={passwordRef} />
+        </div>
+      </div>
+
+      <p className="form-actions">
+        <button type="reset" className="button button-flat">
+          Reset
+        </button>
+        <button className="button">Login</button>
+      </p>
+    </form>
+  );
+}
---
+++ 17 Forms User Input/src/components/StateLogin.jsx
@@ -0,0 +1,66 @@
+import Input from './Input';
+import { isEmail, isNotEmpty, hasMinLength } from '../util/validation';
+import { useInput } from '../hooks/useInput';
+
+export default function Login() {
+  const {
+    value: emailValue,
+    handleInputBlur: handleEmailBlur,
+    handleInputChange: handleEmailChange,
+    hasError: emailHasError,
+  } = useInput('', (value) => isEmail(value) && isNotEmpty(value));
+  const {
+    value: passwordValue,
+    handleInputBlur: handlePasswordBlur,
+    handleInputChange: handlePasswordChange,
+    hasError: passwordHasError,
+  } = useInput('', (value) => hasMinLength(value, 6));
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (emailHasError || passwordHasError) {
+      return;
+    }
+
+    console.log(enteredValue);
+    setEnteredValue({
+      email: '',
+      passward: '',
+    });
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2>Login</h2>
+
+      <div className="control-row">
+        <Input
+          label="Email"
+          id="email"
+          type="email"
+          name="email"
+          onChange={handleEmailChange}
+          onBlur={handleEmailBlur}
+          value={emailValue}
+          error={emailHasError && 'Please enter a valid email!'}
+        />
+        <Input
+          label="Password"
+          id="password"
+          type="password"
+          name="password"
+          onChange={handlePasswordChange}
+          onBlur={handlePasswordBlur}
+          value={passwordValue}
+          error={passwordHasError && 'Please enter a valid password!!'}
+        />
+      </div>
+
+      <p className="form-actions">
+        <button className="button button-flat">Reset</button>
+        <button className="button">Login</button>
+      </p>
+    </form>
+  );
+}
